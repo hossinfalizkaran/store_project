ee653e856cbed8b4225b8622d49adb2c83672fb5
hossinfalizkaran <iran581@gmail.com>
2025-08-04 09:46:15 +0330

    Fix missing authenticate import in views.py

--- accounting/views.py
+++ accounting/views.py
@@ -2,6 +2,7 @@
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.db.models import Max, Q
 from django.core.paginator import Paginator, EmptyPage
